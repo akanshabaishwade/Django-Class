23c9e3b9e53fa9635e8e38e6080da0c91d7f5d9d
akanshabaishwade <akanshabaishwade@gmail.com>
2024-05-29 09:29:35 +0530

    [Add] Day 1

---
+++ Day1/Day1.py
@@ -0,0 +1,35 @@
+# python -m venv venv_name
+# -r
+# pip install Django==5.0.6
+# django-admin
+# django-admin startproject myproject
+# django-admin startapp myapp
+'''
+Project 
+
+command - django-admin startproject myproject
+
+1. In Django, a project is a collection of settings for an instance of Django,
+   including database configuration, Django-specific options, and application-specific settings.
+   It represents a complete web application.
+
+2. settings.py
+
+3. A project is typically the top-level directory that contains all the files and directories needed 
+   for a Django application to run.
+
+4. a project's default file names include settings.py (for project settings), urls.py (for URL patterns),
+ wsgi.py (for WSGI deployment), asgi.py (for ASGI deployment), manage.py (for project management)
+
+'''
+
+'''
+App
+
+command - django-admin startapp myapp 
+
+1. In a Django app, the default file names include init.py (for package recognition),
+ models.py (for database models), views.py (for view functions), 
+ admin.py (for admin interface registration), and apps.py (for app configuration), tests.py.
+
+'''
