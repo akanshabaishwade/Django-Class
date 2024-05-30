173a58d126f4e9a1f1144d5aea481041d5313da0
akanshabaishwade <akanshabaishwade@gmail.com>
2024-05-30 09:18:20 +0530

    [Add] Day 2

---
+++ Day2/Day2.py
@@ -0,0 +1,32 @@
+'''
+Flask and Django:
+
+Flask vs Django =
+Flask and Django are both popular Python web frameworks, but they different in their design 
+philosophies and complexity. 
+
+Flask = 
+Flask is a microframework that is simple and lightweight, making it suitable for 
+small-scale applications and prototypes.
+It provides flexibility and allows developers to choose the components they need.
+
+Django =
+Django is a full-featured web framework that is designed for large-scale applications.
+ full-stack framework that includes everything needed to build web applications out of the box.
+   It follows the "batteries-included" philosophy, providing a set of components for common web
+   development tasks such as authentication, routing, and templating.
+'''
+
+'''
+Steps for creating Django Project
+
+mkdir djangoclass
+cd path\to\your\djangoclass
+python -m venv venv (-m = make)
+1.Create a directory named venv in your current working directory.
+2.Inside the venv directory, it will create the necessary subdirectories
+ and files to support a virtual environment, including the Python interpreter
+  and a local site-packages directory.
+venv\Scripts\activate
+
+'''
