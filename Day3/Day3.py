c6ad10696d3f7fc31f05d35a61204b25b6dbd648
akanshabaishwade <akanshabaishwade@gmail.com>
2024-05-31 19:00:56 +0530

    [Add] Day 3 and Day 4

---
+++ Day3/Day3.py
@@ -0,0 +1,14 @@
+'''
+
+create venv = python -m venv venv
+activate the environment =
+windows - venvname\Scripts\activate.bat
+ubuntu - source venvname/bin/activate
+
+install - pip install django
+py --version
+django-admin --version
+django-admin startproject projectname
+python manage.py runserver
+open - http://127.0.0.1:8000/
+'''
