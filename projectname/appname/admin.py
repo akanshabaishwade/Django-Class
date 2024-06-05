65a65f09e902af3ac00da911fcf8177275b1f859
akanshabaishwade <akanshabaishwade@gmail.com>
2024-06-05 09:03:58 +0530

    [Add] Day 6

--- projectname/appname/admin.py
+++ projectname/appname/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-# from .models import *
-from .models import Student, ModelName
+from .models import *
 # Register your models here.
 
 admin.site.register(Student)
+admin.site.register(ModelName)
+admin.site.register(AllFieldType)
+
