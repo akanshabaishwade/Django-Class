08ce0896642c3792293336ddcb26ddd6ee188e2c
akanshabaishwade <akanshabaishwade@gmail.com>
2024-06-12 08:17:33 +0530

    [Add] Day 10

--- projectname/appname/urls.py
+++ projectname/appname/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('home2/', views.home2, name='home'),
+    
+    
 ]
--- projectname/appname/views.py
+++ projectname/appname/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+\
 
 
 
 def home(request):
     return HttpResponse("Hello, this is a dynamic web page!")
+
+
+def home2(request):
+    context = {'message': 'Hello, this is a dynamic web page with a template!'}
+    return render(request, 'index.html', context)
